d64c203ff7d5323a3d23e56c98cbe3e01188ad95
rezowansifat <navidrezowansifat@gmail.com>
2024-05-27 17:15:32 +0600

    public data restore

--- config/database.js
+++ config/database.js
@@ -2,20 +2,10 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-// const connectDatabase = () => {
-//   mongoose.connect(process.env.DB_URI).then((data) => {
-//     console.log(`Mongodb connected with server ${data.connection.host}`);
-//   });
-// };
-
 const connectDatabase = () => {
-  mongoose
-    .connect(
-      "mongodb+srv://datapollex:dpxultra50@datapollex.12ihweq.mongodb.net/DataPollex"
-    )
-    .then((data) => {
-      console.log(`Mongodb connected with server ${data.connection.host}`);
-    });
+  mongoose.connect(process.env.DB_URI).then((data) => {
+    console.log(`Mongodb connected with server ${data.connection.host}`);
+  });
 };
 
 module.exports = connectDatabase;
